428b7c5d074b73e03396af8e2915ad2c4ef61821
Yasuhiro Matsuda <yasuhirom@wepay.com>
2019-09-11 09:26:56 -0700

    fix projectName

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'waltz',
   organizationName: 'wepay',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
