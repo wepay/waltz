74fb408b26b9d2a36d683e2cd9b593a08c84e0de
William Pursell <williamp@wepay.com>
2019-10-30 19:04:08 +0000

    Remove CLASSPATH and JAVA_TOOL_OPTIONS from environment

--- bin/demo-bank-app.sh
+++ bin/demo-bank-app.sh
@@ -4,7 +4,12 @@ DIR=$(dirname $0)
 
 $DIR/../gradlew --console plain -q copyLibs
 
-export CLASSPATH=$(echo waltz-demo/build/libs/*.jar | tr ' ' :)
-export JAVA_TOOL_OPTIONS=-Dlog4j.configuration=file:config/log4j.properties
+unset CLASSPATH
+unset JAVA_TOOL_OPTIONS
+CLASSPATH=$(echo waltz-demo/build/libs/*.jar | tr ' ' :)
+JAVA_TOOL_OPTIONS=-Dlog4j.configuration=file:config/log4j.properties
 
-java com.wepay.waltz.demo.DemoBankApp config/local-docker/demo-app.yml
+java \
+	$JAVA_TOOL_OPTIONS \
+	-cp "$CLASSPATH" \
+	com.wepay.waltz.demo.DemoBankApp config/local-docker/demo-app.yml
