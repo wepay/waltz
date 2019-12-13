dae4cae974697a7e22875b27531c0a857ec09e7e
Hongyi Wang <hongyiw@wepay.com>
2019-12-13 10:02:39 -0800

    DI-3047; Update Waltz integration tests regarding ducktape change

--- waltz-test/src/main/python/waltz_ducktape/tests/validation/smoke_test.py
+++ waltz-test/src/main/python/waltz_ducktape/tests/validation/smoke_test.py
@@ -1,6 +1,7 @@
 from random import randrange
 from ducktape.mark.resource import cluster
 from ducktape.mark import parametrize
+from ducktape.cluster.cluster_spec import ClusterSpec
 from waltz_ducktape.tests.produce_consume_validate import ProduceConsumeValidateTest
 from waltz_ducktape.tests.validation.node_bounce_scheduler import NodeBounceScheduler
 
@@ -11,17 +12,22 @@ class SmokeTest(ProduceConsumeValidateTest):
     and client nodes. Fire transactions while turning things off and on,
     to ensure Waltz can recover from expected failure.
     """
+    MIN_CLUSTER_SPEC = ClusterSpec.from_list([
+        {'cpu':1, 'mem':'1GB', 'disk':'25GB', 'additional_disks':{'/dev/sdb':'100GB'}, 'num_nodes':3},
+        {'cpu':1, 'mem':'3GB', 'disk':'15GB', 'num_nodes':2},
+        {'cpu':1, 'mem':'1GB', 'disk':'25GB', 'num_nodes':1}])
+
     def __init__(self, test_context):
         super(SmokeTest, self).__init__(test_context=test_context)
 
-    @cluster(nodes_spec={'storage':3, 'server':2, 'client':1})
+    @cluster(cluster_spec=MIN_CLUSTER_SPEC)
     @parametrize(num_active_partitions=1, txn_per_client=500, num_clients=10, interval=120, timeout=240)
     @parametrize(num_active_partitions=4, txn_per_client=500, num_clients=10, interval=120, timeout=240)
     def test_produce_consume_no_torture(self, num_active_partitions, txn_per_client, num_clients, interval, timeout):
         validation_cmd = self.client_cli.validate_txn_cmd(num_active_partitions, txn_per_client, num_clients, interval)
         self.run_produce_consume_validate(lambda: self.simple_validation_func(validation_cmd, timeout))
 
-    @cluster(nodes_spec={'storage':3, 'server':2, 'client':1})
+    @cluster(cluster_spec=MIN_CLUSTER_SPEC)
     @parametrize(num_active_partitions=1, txn_per_client=500, num_clients=10, interval=120, timeout=480)
     @parametrize(num_active_partitions=4, txn_per_client=500, num_clients=10, interval=120, timeout=480)
     def test_produce_consume_while_bouncing_storage_nodes(self, num_active_partitions, txn_per_client, num_clients, interval, timeout):
@@ -30,7 +36,7 @@ class SmokeTest(ProduceConsumeValidateTest):
                                                               lambda: self._bounce_storage_nodes(3))
         assert "exception" not in validation_result.lower(), "Test failed with exception:\n{}".format(validation_result)
 
-    @cluster(nodes_spec={'storage':3, 'server':2, 'client':1})
+    @cluster(cluster_spec=MIN_CLUSTER_SPEC)
     @parametrize(num_active_partitions=1, txn_per_client=500, num_clients=2, interval=120, timeout=240)
     @parametrize(num_active_partitions=4, txn_per_client=500, num_clients=2, interval=120, timeout=240)
     def test_produce_consume_while_killing_a_server_node(self, num_active_partitions, txn_per_client, num_clients, interval, timeout):
