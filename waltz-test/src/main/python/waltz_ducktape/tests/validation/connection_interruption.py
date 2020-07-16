702bfb28c40fa483cd019fee3524bdef7a83640e
Tomas Hrdlovics <tomash@wepay.com>
2020-07-16 09:21:42 -0700

    changes: - interruption in seconds instead of milliseconds - changes according to codebase conventions

--- waltz-test/src/main/python/waltz_ducktape/tests/validation/connection_interruption.py
+++ waltz-test/src/main/python/waltz_ducktape/tests/validation/connection_interruption.py
@@ -7,31 +7,31 @@ class ConnectionInterruption(Thread):
     A scheduler to drop incoming traffic to a port periodically.
     """
 
-    def __init__(self, service, interruption_length, no_of_interruptions, service_node_index):
+    def __init__(self, service, interruption_length, num_of_interruptions, service_node_index):
         """
             Construct a new 'ConnectionInterruption' object.
 
             :param service: The service having incoming traffic to a port blocked
             :param interruption_length: The interval(milliseconds) between two interruptions and the length of each interruption
-            :param no_of_interruptions: Total number of network connection interruptions
+            :param num_of_interruptions: Total number of network connection interruptions
             :param service_node_index: A service node used for this test
         """
         Thread.__init__(self)
         self.service = service
         self.interruption_length = interruption_length
-        self.interruptions = no_of_interruptions
+        self.num_of_interruptions = num_of_interruptions
         self.service_node_index = service_node_index
 
     def run(self):
         node = self.service.nodes[self.service_node_index]
-        for interruption in range(self.interruptions):
+        for interruption in range(self.num_of_interruptions):
             # let waltz server receive transactions as usual
-            sleep(self.interruption_length/1000)
+            sleep(self.interruption_length)
 
             # disable connection on port
             self.service.logger.info("Closing a port")
             node.account.ssh_capture("sudo iptables -I INPUT -p tcp --destination-port {} -j DROP".format(self.service.port))
-            sleep(self.interruption_length / 1000)
+            sleep(self.interruption_length)
 
             # enable connection on port
             self.service.logger.info("Opening a port")
